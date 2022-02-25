377e3caa6e0a94badb24136acb82c7592c12f9a9
Hossam Khader <hossamkhader@gmail.com>
2022-02-25 04:17:56 -0500

    DispatchSend should return the total number of bytes sent over all the streams

--- model/quic-l5-protocol.cc
+++ model/quic-l5-protocol.cc
@@ -198,7 +198,7 @@ QuicL5Protocol::DispatchSend (Ptr<Packet> data)
         {
           NS_LOG_INFO (
             "Sending data on stream " << (*jt)->GetStreamId ());
-          sentData = (*jt)->Send ((*it));
+          sentData += (*jt)->Send ((*it));
           ++it;
         }
     }
